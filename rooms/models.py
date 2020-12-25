c8dd540825242270c56760aa87480ae9844d44d0
Jin-hw <70885954+Jin-hw@users.noreply.github.com>
2020-12-25 23:49:52 +0900

    Add Room Photo

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
 
 
 class AbstractItem(core_models.TimeStampedModel):
@@ -19,7 +18,46 @@ class AbstractItem(core_models.TimeStampedModel):
 
 class RoomType(AbstractItem):
 
-    pass
+    """ RoomType Model Definition """
+
+    class Meta:
+        verbose_name = "Room Type"
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """Facility Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Model Definition """
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -32,11 +70,19 @@ class Room(core_models.TimeStampedModel):
     city = models.CharField(max_length=80)
     price = models.IntegerField()
     address = models.CharField(max_length=140)
+    guests = models.IntegerField(blank=True)
     beds = models.IntegerField()
     bedrooms = models.IntegerField()
     baths = models.IntegerField()
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    # CASCADE는 상위항목을 삭제하면 하위항목도 삭제됨
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
+
+    def __str__(self):
+        return self.name
