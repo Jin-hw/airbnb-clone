db3a136b9ec566f98e04c3e369a74270edb54e82
Jin-hw <70885954+Jin-hw@users.noreply.github.com>
2021-01-03 23:28:09 +0900

    Change View

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,8 @@
-from math import ceil
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
-
+""" #Without assist from Django
 # config-settings.py의 templates-DIRS에 template 경로를 등록한다
 # view의 이름은 core-urls의 view 이름과 같아야한다.
 def all_rooms(request):
@@ -12,10 +12,28 @@ def all_rooms(request):
     limit = page_size * page
     offset = limit - page_size
     all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = models.Room.objects.count() / page_size
+    page_count = ceil(models.Room.objects.count() / page_size)
     return render(
         request,
         "rooms/home.html",
-        context={"rooms": all_rooms, "page": page, "page_count": ceil(page_count)},
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count + 1),
+        },
     )
     # html은 templates 내부의 html 파일 이름과 같아야한다.
+"""
+
+
+def all_rooms(request):
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")
