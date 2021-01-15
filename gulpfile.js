a9585ac2047538701bf002fddd49a569e8f16125
Jin-hw <70885954+Jin-hw@users.noreply.github.com>
2021-01-15 22:38:11 +0900

    Login with CSS

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,13 @@ const css = () => {
     const minify = require("gulp-csso");
     sass.compiler = require("node-sass");
     return gulp
-    .src("assets/scss/styles.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(postCSS([
-        require("tailwindcss"),
-        require("autoprefixer")
-    ])).pipe(minify())
-    .pipe(gulp.dest("static/css"));
+        .src("assets/scss/styles.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(postCSS([
+            require("tailwindcss"),
+            require("autoprefixer")
+        ])).pipe(minify())
+        .pipe(gulp.dest("static/css"));
 };
 
 exports.default = css;
